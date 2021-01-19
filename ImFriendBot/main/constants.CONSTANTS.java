d15915ea92fb5f9393173404870ec467dc979033
Mechanic <64973921+iamm3chanic@users.noreply.github.com>
2021-01-19 13:05:34 +0300

    Create constants.CONSTANTS.java

---
+++ ImFriendBot/main/constants.CONSTANTS.java
@@ -0,0 +1,9 @@
+package constants;
+public class CONSTANTS {
+    public static int SENDER_SLEEP_TIME;
+    public static int WAIT_FOR_NEW_MESSAGE_DELAY;
+    public static int MILLISEC_IN_SEC = 1000;
+    public static int PRIORITY_FOR_SENDER = 1;
+    public static int PRIORITY_FOR_RECEIVER = 1;
+    public static String BOT_ADMIN = "358377512";
+}
