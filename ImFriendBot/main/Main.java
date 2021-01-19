995b553d7b8fe559bb302fc65a4c50b59e6d5306
Mechanic <64973921+iamm3chanic@users.noreply.github.com>
2021-01-19 13:00:12 +0300

    Update Main.java

--- ImFriendBot/main/Main.java
+++ ImFriendBot/main/Main.java
@@ -1,23 +1,17 @@
 //import Bot;
 //import MessageReceiver;
 //import MessageSender;
-
+import constants.CONSTANTS;
 import org.apache.log4j.Logger;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.api.methods.send.SendMessage;
 
 public class Main {
-   // private static final Logger log = Logger.getLogger(Main.class);
-    private static final int PRIORITY_FOR_SENDER = 1;
-    private static final int PRIORITY_FOR_RECEIVER = 3;
-    private static final String BOT_ADMIN = "12345";
     public static Bot bot;
-    //private Bot bot;
-    //private String chatId;
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
-        Bot friend_bot = new Bot("ImFriendBot", "tokentokentoken");
+        Bot friend_bot = new Bot("ImFriendBot", "111AAAAA");
 
         MessageReceiver messageReceiver = new MessageReceiver(friend_bot);
         MessageSender messageSender = new MessageSender(friend_bot);
@@ -32,13 +26,13 @@ public class Main {
         Thread receiver = new Thread(messageReceiver);
         receiver.setDaemon(true);
         receiver.setName("MsgReceiver");
-        receiver.setPriority(PRIORITY_FOR_RECEIVER);
+        receiver.setPriority(CONSTANTS.PRIORITY_FOR_RECEIVER);
         receiver.start();
 
         Thread sender = new Thread(messageSender);
         sender.setDaemon(true);
         sender.setName("MsgSender");
-        sender.setPriority(PRIORITY_FOR_SENDER);
+        sender.setPriority(CONSTANTS.PRIORITY_FOR_SENDER);
         sender.start();
 
         sendStartReport(friend_bot);
@@ -48,8 +42,8 @@ public class Main {
 
     private static void sendStartReport(Bot bot) {
         SendMessage sendMessage = new SendMessage();
-        sendMessage.setChatId(BOT_ADMIN);
-        sendMessage.setText("Деплой произошел успешно! Version normal6.");
+        sendMessage.setChatId(CONSTANTS.BOT_ADMIN);
+        sendMessage.setText("Деплой произошел успешно! Version normal7.");
         bot.sendQueue.add(sendMessage);
     }
 }
